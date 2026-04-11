ad2672bc937fd9161a1c0f1c123634c93537ef1a
Domi <rzmsdo@gmail.com>
2026-04-11 23:57:37 +0200

    Continuacion conf Hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 
 import './index.css'
 import Navbar from './components/Navbar'
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,28 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import SplitType from "split-type";
+import videoHero from '../assets/videoHero.mp4';
+
 
 
 const Hero = () => {
+
+    const text = new SplitType(".bodyUp p:nth-of-type(1)", { types: 'words,chars' });
+
+
+
+
     return (
     <>
         <section id="hero" className="container mx-auto h-full pt-20">
-            <h2 className="will-fade">"Soluciones que impulsan tu negocio"</h2>
-            <div className="body">
+            <h2 className="will-fade z-50">¿Estás aprovechando todo el potencial digital de tu negocio?</h2>
+            <div className='bodyUp'>
+                <p>Mandarina Projects</p>
+                <p>Soluciones digitales diseñadas para crecer contigo.</p>
+                <p>Ofrecemos desde el desarrollo de sitios web hasta plataformas SaaS, optimización SEO y creación de productos digitales.</p>
+                <p>Construimos tecnología que impulsa tu negocio.</p>
+            </div>
+            <div className="bodyDown">
             <div className="content">
              <div className="space-y-5 text-xl hidden md:block">
             <p>Tu web</p>
@@ -14,19 +31,26 @@ const Hero = () => {
             </p>
             
             </div>
-            <div className="sas">
+            <div className="sas ">
            
-            <p className="subtitle">
+            <p className="subtitle text-center">
                 Conecta tu negocio con <br/>tus clientes y
             </p>
-             <p>maximiza tus resultados.
+             <p className="text-center">maximiza tus resultados.
             </p>
            
             </div>
             </div>
         </div>
+        <div className="video">
+            <video src={videoHero}
+                autoPlay
+                playsInline 
+                muted 
+                preload="auto"
+                />
+        </div>
         </section>
-         
   </>
         )
 }
