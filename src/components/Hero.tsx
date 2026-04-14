c6caa243aa4c8697bae5cc0f715154c4518b9863
Domingo Ramírez Morales <128168900+Rzmsdo@users.noreply.github.com>
2026-04-14 12:44:24 +0000

    Hero responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,7 @@ import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import SplitType from "split-type";
 import videoHero from '../assets/videoHero.mp4';
+import imgHero from '../assets/imgHero.png';
 
 
 
@@ -9,18 +10,36 @@ const Hero = () => {
 
     const text = new SplitType(".bodyUp p:nth-of-type(1)", { types: 'words,chars' });
 
-
+        useGSAP(() => {
+            const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
+            tl.from(".will-fade", { opacity: 0, y: 20, stagger: 0.3 })
+                .from(text.chars, { opacity: 0, y: 50, stagger: 0.05 }, "-=1")
+                .from(".bodyUp p:nth-of-type(2)", { opacity: 0, y: 20 }, "-=1")
+                .from(".bodyUp p:nth-of-type(3)", { opacity: 0, y: 20 }, "-=0.8")
+                .from(".bodyUp p:nth-of-type(4)", { opacity: 0, y: 20 }, "-=0.8")
+                .from(".bodyDown .content", { opacity: 0, y: 30, stagger: 0.3 }, "-=1");
+        });
 
 
     return (
     <>
-        <section id="hero" className="container mx-auto h-full pt-20">
+        <section id="hero" className="mx-auto w-full">
             <h2 className="will-fade z-50">¿Estás aprovechando todo el potencial digital de tu negocio?</h2>
-            <div className='bodyUp'>
-                <p>Mandarina Projects</p>
-                <p>Soluciones digitales diseñadas para crecer contigo.</p>
-                <p>Ofrecemos desde el desarrollo de sitios web hasta plataformas SaaS, optimización SEO y creación de productos digitales.</p>
-                <p>Construimos tecnología que impulsa tu negocio.</p>
+            <p className="name">Mandarina Projects</p>
+            <div className="centroHero">
+                
+                <div className="bloqHeIzda will-fade">
+                    <img src={imgHero} alt="Hero" className="heroImage" />
+                </div>
+
+                <div className="bloqHeDcha">
+                    <div className='bodyUp'>
+                        
+                        <p>Soluciones digitales diseñadas para crecer contigo.</p>
+                        <p>Ofrecemos desde el desarrollo de sitios web hasta plataformas SaaS, optimización SEO y creación de productos digitales.</p>
+                        <p>Construimos tecnología que impulsa tu negocio.</p>
+                    </div>
+                </div>
             </div>
             <div className="bodyDown">
             <div className="content">
@@ -34,7 +53,7 @@ const Hero = () => {
             <div className="sas ">
            
             <p className="subtitle text-center">
-                Conecta tu negocio con <br/>tus clientes y
+                Conecta tu negocio  <br/>con tus clientes y
             </p>
              <p className="text-center">maximiza tus resultados.
             </p>
